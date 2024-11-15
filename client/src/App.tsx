6ae6abe1854585c9625f28f853fd0a0acf3df062
antoinebcx <antoine.branchoux@gmail.com>
2024-11-15 21:57:50 +0100

    color mode system detection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,20 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
-import { Box } from '@mui/material';
+import { Box, useMediaQuery } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import { createAppTheme } from './theme';
 import MermaidEditor from './components/MermaidEditor';
 import NavBar from './components/NavBar';
 
 function App() {
-  const [mode, setMode] = useState<'light' | 'dark'>('light');
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const [mode, setMode] = useState<'light' | 'dark'>(prefersDarkMode ? 'dark' : 'light');
+  useEffect(() => {
+    setMode(prefersDarkMode ? 'dark' : 'light');
+  }, [prefersDarkMode]);
+
   const theme = useMemo(() => createAppTheme(mode), [mode]);
-  
+
   const toggleTheme = () => {
     setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
   };
