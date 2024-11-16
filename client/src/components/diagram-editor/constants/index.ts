ea843cddc4a623a3d61f7ffa911b85c792dbc9a4
antoinebcx <antoine.branchoux@gmail.com>
2024-11-16 15:03:32 +0100

    create editor constants

---
+++ client/src/components/diagram-editor/constants/index.ts
@@ -0,0 +1,11 @@
+export const NAVBAR_HEIGHT = 64;
+export const MAX_HISTORY_LENGTH = 100000;
+export const DEFAULT_DIAGRAM = `graph TD
+    A(Email) --> C[Process Message]
+    B(SMS) --> C
+    C --> D{Valid Order?}
+    D -->|Yes| E[Create Order]
+    D -->|No| F[Reject Request]
+    E --> G(Send Confirmation)
+    E --> H(Update Inventory)
+    F --> I(Send Rejection Notice)`;
