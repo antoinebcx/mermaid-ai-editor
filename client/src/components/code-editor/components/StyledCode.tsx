c1e01652ec6769db0580453670fee1dc0506e03d
antoinebcx <antoine.branchoux@gmail.com>
2024-11-16 15:41:24 +0100

    define global icon colors

--- client/src/components/code-editor/components/StyledCode.tsx
+++ client/src/components/code-editor/components/StyledCode.tsx
@@ -14,11 +14,6 @@ export const CopyButton = styled(IconButton)(({ theme }) => ({
   top: '1rem',
   right: '1rem',
   zIndex: 20,
-  backgroundColor: theme.palette.mode === 'dark' ? 'rgba(255, 255, 255, 0)' : 'rgba(0, 0, 0, 0)',
-  '&:hover': {
-    backgroundColor: theme.palette.mode === 'dark' ? 'rgba(255, 255, 255, 0.1)' : 'rgba(0, 0, 0, 0.1)',
-  },
-  opacity: 0.6
 }));
 
 export const EditorTextarea = styled('textarea')(({ theme }) => ({
