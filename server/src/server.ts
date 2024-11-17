9e642f6ff12bd302cf469e975b263e6cfcab8140
antoinebcx <antoine.branchoux@gmail.com>
2024-11-17 21:27:05 +0100

    origins in server env variables

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,9 +7,21 @@ import { errorHandler } from './middleware/error.middleware';
 
 const app = express();
 
+const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : [];
+
 app.use(cors({
-  origin: config.cors.origin
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
 }));
+
 app.use(express.json());
 
 app.use('/api/diagrams', diagramRoutes);
